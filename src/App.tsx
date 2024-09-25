2ad00b25ba443bb60862414f03646aaba8044e7f
Utkarsh Singh <utkarsh.singh6113@gmail.com>
2024-09-25 23:17:38 +0530

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { cn } from "./lib/utils";
+import AnimatedGridPattern from "./components/magicui/animated-grid-pattern";
+
+const App = () => {
+  return (
+    <div className="relative flex h-screen w-full items-center justify-center overflow-hidden rounded-lg border bg-background p-20 md:shadow-xl">
+      <p className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white">
+        Animated Grid Pattern
+      </p>
+      <AnimatedGridPattern
+        numSquares={30}
+        maxOpacity={0.1}
+        duration={3}
+        repeatDelay={1}
+        className={cn(
+          "[mask-image:radial-gradient(100vh_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+        )}
+      />
+      <div className='h-[100px] w-[100px] rounded-full bg-black ml-4'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
